23bbeb6d58a61ddd0eba773a37b66b73c9e7b23a
Nikola Kolev <nikolakolev712@gmail.com>
2022-06-22 22:05:40 +0300

    .

--- topki.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for i in os.listdir('C:\\Users\\nikol\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages\\moviepy\\video\\fx'):
-    hui = i.split('.')[0]
-    print(f'from moviepy.video.fx.{hui} import {hui}')
